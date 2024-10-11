0fdbe1d1ae7700d092d8947c27b4e0a7cf317efe
Ivan Nur Ilham Syah <work.ilhams@gmail.com>
2024-10-11 09:56:20 +0700

    setup project

---
+++ Muvi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Muvi
+//
+//  Created by Ivan Nur Ilham Syah on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
