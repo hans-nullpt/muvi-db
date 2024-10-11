c28d97cf8a3ebcf283049967e39be637037072f2
Ivan Nur Ilham Syah <work.ilhams@gmail.com>
2024-10-11 16:37:12 +0700

    feat: create domain files

---
+++ Muvi/Domain/Entities/Movie.swift
@@ -0,0 +1,41 @@
+//
+//  Movie.swift
+//  Muvi
+//
+//  Created by Ivan Nur Ilham Syah on 11/10/24.
+//
+
+struct Movie: Codable {
+
+    var popularity: Double?
+    var voteCount: Int?
+    var posterPath: String?
+    var releaseDate: String?
+    var overview: String?
+    var voteAverage: Double?
+    var backdropPath: String?
+    var id: Int?
+    var adult: Bool?
+    var video: Bool?
+    var originalLanguage: String?
+    var title: String?
+    var genreIds: [Int]?
+    var originalTitle: String?
+
+    enum CodingKeys: String, CodingKey {
+        case popularity = "popularity"
+        case voteCount = "vote_count"
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case overview = "overview"
+        case voteAverage = "vote_average"
+        case backdropPath = "backdrop_path"
+        case id = "id"
+        case adult = "adult"
+        case video = "video"
+        case originalLanguage = "original_language"
+        case title = "title"
+        case genreIds = "genre_ids"
+        case originalTitle = "original_title"
+    }
+}
