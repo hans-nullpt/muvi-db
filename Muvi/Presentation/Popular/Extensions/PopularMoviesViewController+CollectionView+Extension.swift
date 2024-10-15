155167ea96f589a934b265b97a10af77125ef702
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-15 21:01:53 +0700

    fix: refactor collection view config code, change image content mode to aspect fill

--- Muvi/Presentation/Popular/Extensions/PopularMoviesViewController+CollectionView+Extension.swift
+++ Muvi/Presentation/Popular/Extensions/PopularMoviesViewController+CollectionView+Extension.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import UIKit
+
+extension PopularMoviesViewController {
+  internal func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createMovieGridFlowLayout())
+    
+    view.addSubview(collectionView)
+    
+    collectionView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+    
+    collectionView.register(MovieGridCell.nib(), forCellWithReuseIdentifier: MovieGridCell.reusableId)
+    collectionView.backgroundColor = .systemBackground
+  }
+  
+  internal func createMovieGridFlowLayout() -> UICollectionViewFlowLayout {
+    let width = view.bounds.width
+    let padding: CGFloat = 20
+    let minimumItemSpacing: CGFloat = 5
+    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+    let itemWidth = availableWidth / 2
+    
+    let flowLayout = UICollectionViewFlowLayout()
+    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding * 2, right: padding)
+    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 88)
+    
+    return flowLayout
+  }
+  
+  internal func configureDataSource() {
+    datasource = MovieDataSource(collectionView: collectionView) { collectionView, indexPath, movie in
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieGridCell.reusableId, for: indexPath) as? MovieGridCell
+      
+      guard let cell else { return UICollectionViewCell() }
+      
+      cell.updateData(with: movie)
+      
+      return cell
+    }
+    
+  }
+  
+  internal func updateCollectionViewData(
+    with movies: [Movie]
+  ) {
+    var snapshot = NSDiffableDataSourceSnapshot<Int, Movie>()
+    snapshot.appendSections([0])
+    snapshot.appendItems(movies)
+    
+    DispatchQueue.main.async { [weak self] in
+      guard let self else { return }
+      
+      self.datasource.apply(snapshot)
+      
+    }
+    
+  }
+  
+}
--- Muvi/Presentation/Popular/PopularMoviesViewController.swift
+++ Muvi/Presentation/Popular/PopularMoviesViewController.swift
@@ -6,96 +6,38 @@
 //
 
 import UIKit
+import RxSwift
 
 class PopularMoviesViewController: UIViewController {
   typealias MovieDataSource = UICollectionViewDiffableDataSource<Int, Movie>
   
-  private let label = UILabel()
   internal var collectionView: UICollectionView!
   internal var datasource: MovieDataSource!
+  
+  internal let viewModel: PopularMovieListViewModel
+  internal let disposeBag = DisposeBag()
+  
+  init(viewModel: PopularMovieListViewModel) {
+    self.viewModel = viewModel
+    
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    configureLabel()
     configureCollectionView()
     configureDataSource()
     
-    updateCollectionViewData(with: [Movie(id: 1), Movie(id: 2), Movie(id: 3)])
-  }
-  
-  private func configureLabel() {
-    view.addSubview(label)
-    
-    label.font = .preferredFont(forTextStyle: .title2)
-    label.textColor = .white
-    label.text = "Popular Movies"
-    label.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-    ])
-    
-    label.snp.makeConstraints { make in
-      make.center.equalToSuperview()
-    }
-  }
-  
-  internal func configureCollectionView() {
-    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createMovieGridFlowLayout())
-    
-    view.addSubview(collectionView)
-    
-    collectionView.snp.makeConstraints { make in
-      make.edges.equalToSuperview()
+    Task {
+      try await viewModel.getPopularMovies()
     }
     
-    collectionView.register(MovieGridCell.nib(), forCellWithReuseIdentifier: MovieGridCell.reusableId)
-    
-  }
-  
-  internal func createMovieGridFlowLayout() -> UICollectionViewFlowLayout {
-    let width = view.bounds.width
-    let padding: CGFloat = 12
-    let minimumItemSpacing: CGFloat = 10
-    let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-    let itemWidth = availableWidth / 2
-    
-    let flowLayout = UICollectionViewFlowLayout()
-    flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-    flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-    
-    return flowLayout
-  }
-  
-  internal func configureDataSource() {
-    datasource = MovieDataSource(collectionView: collectionView) { collectionView, indexPath, movie in
-      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieGridCell.reusableId, for: indexPath) as? MovieGridCell
-      
-      guard let cell else { return UICollectionViewCell() }
-      
-      cell.updateData(with: movie)
-      
-      return cell
-    }
-    
-  }
-  
-  internal func updateCollectionViewData(
-    with movies: [Movie]
-  ) {
-    var snapshot = NSDiffableDataSourceSnapshot<Int, Movie>()
-    snapshot.appendSections([0])
-    snapshot.appendItems(movies)
-    
-    DispatchQueue.main.async { [weak self] in
-      guard let self else { return }
-      
-      self.datasource.apply(snapshot)
-      
-    }
-    
+    observeMovies()
   }
   
 }
