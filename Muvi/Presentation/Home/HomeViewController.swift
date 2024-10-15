3ca8b14ef3739cba24096fe95c653592d0651a20
Ivan Nur Ilham Syah <work.ilhams@gmail.com>
2024-10-15 09:20:48 +0700

    fix: change title label to lazy var

--- Muvi/Presentation/Home/HomeViewController.swift
+++ Muvi/Presentation/Home/HomeViewController.swift
@@ -9,110 +9,113 @@ import UIKit
 import RxSwift
 
 enum MovieSection: CaseIterable, Hashable {
-    case topRated
-    case popular
-    case upcoming
-    
-    var title: String {
-        switch self {
-            
-        case .topRated:
-            ""
-        case .popular:
-            "Popular"
-        case .upcoming:
-            "Coming Soon"
-        }
+  case topRated
+  case popular
+  case upcoming
+  
+  var title: String {
+    switch self {
+      
+    case .topRated:
+      ""
+    case .popular:
+      "Popular"
+    case .upcoming:
+      "Coming Soon"
     }
+  }
 }
 
 class HomeViewController: UIViewController {
-    typealias MovieDataSource = UICollectionViewDiffableDataSource<MovieSection, Movie>
+  typealias MovieDataSource = UICollectionViewDiffableDataSource<MovieSection, Movie>
+  
+  internal let viewModel: MovieListViewModel
+  internal let disposeBag = DisposeBag()
+  
+  internal var collectionView: UICollectionView!
+  internal lazy var titleLabel: UILabel = {
+    let label = UILabel()
     
-    internal let viewModel: MovieListViewModel
-    internal let disposeBag = DisposeBag()
+    let muviLabelAttr: [NSAttributedString.Key: Any] = [
+      .font: UIFont.systemFont(ofSize: 32, weight: .bold)
+    ]
     
-    internal var collectionView: UICollectionView!
+    let dbLabelAttr = [
+      .font: UIFont.systemFont(ofSize: 32, weight: .bold),
+      NSAttributedString.Key.foregroundColor: UIColor.systemYellow
+    ]
     
-    internal var datasource: MovieDataSource!
+    let muviLabelAttrString = NSMutableAttributedString(string: "Muvi", attributes: muviLabelAttr)
     
-    init(viewModel: MovieListViewModel) {
-        self.viewModel = viewModel
-        
-        super.init(nibName: nil, bundle: nil)
+    let dbLabelAttrString  = NSAttributedString(string: "DB", attributes: dbLabelAttr)
+    
+    muviLabelAttrString.append(dbLabelAttrString)
+    
+    label.attributedText = muviLabelAttrString
+    
+    return label
+  }()
+  
+  internal var datasource: MovieDataSource!
+  
+  init(viewModel: MovieListViewModel) {
+    self.viewModel = viewModel
+    
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configureCollectionView()
+    configureDataSource()
+    
+    Task {
+      try await viewModel.getTopRatedMovies()
+      try await viewModel.getPopularMovies()
+      try await viewModel.getUpcomingMovies()
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    observeTopRatedMovies()
+    observePopularMovies()
+    observeUpcomingMovies()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        configureCollectionView()
-        configureDataSource()
-        
-        Task {
-            try await viewModel.getTopRatedMovies()
-            try await viewModel.getPopularMovies()
-            try await viewModel.getUpcomingMovies()
-        }
-         
-        observeTopRatedMovies()
-        observePopularMovies()
-        observeUpcomingMovies()
-    }
+    let title = UIBarButtonItem()
+    title.customView = titleLabel
+    navigationItem.leftBarButtonItem = title
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        let label = UILabel()
-        
-        let muviLabelAttr: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 32, weight: .bold)
-        ]
-        
-        let dbLabelAttr = [
-            .font: UIFont.systemFont(ofSize: 32, weight: .bold),
-            NSAttributedString.Key.foregroundColor: UIColor.systemYellow
-        ]
-        
-        let muviLabelAttrString = NSMutableAttributedString(string: "Muvi", attributes: muviLabelAttr)
-        
-        let dbLabelAttrString  = NSAttributedString(string: "DB", attributes: dbLabelAttr)
-        
-        muviLabelAttrString.append(dbLabelAttrString)
-        
-        label.attributedText = muviLabelAttrString
-        
-        let title = UIBarButtonItem()
-        title.customView = label
-        navigationItem.leftBarButtonItem = title
-        
-    }
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        collectionView.frame = view.bounds
-    }
-
+    collectionView.frame = view.bounds
+  }
+  
 }
 
 #Preview(traits: .defaultLayout, body: {
-    let datasource = MovieRemoteDataSourceImpl()
-    
-    let repository = MovieRepositoryImpl(remoteDataSource: datasource)
-    
-    let topRatedMoviesUsecase = GetTopRatedMoviesUsecase(repository: repository)
-    let popularMoviesUsecase = GetPopularMoviesUsecase(repository: repository)
-    let upcomingMoviesUsecase = GetUpcomingMoviesUsecase(repository: repository)
-    
-    let viewModel = MovieListViewModel(
-        topRatedMoviesUsecase: topRatedMoviesUsecase,
-        popularMoviesUsecase: popularMoviesUsecase,
-        upcomingMoviesUsecase: upcomingMoviesUsecase
-    )
-    
-    HomeViewController(viewModel: viewModel)
+  let datasource = MovieRemoteDataSourceImpl()
+  
+  let repository = MovieRepositoryImpl(remoteDataSource: datasource)
+  
+  let topRatedMoviesUsecase = GetTopRatedMoviesUsecase(repository: repository)
+  let popularMoviesUsecase = GetPopularMoviesUsecase(repository: repository)
+  let upcomingMoviesUsecase = GetUpcomingMoviesUsecase(repository: repository)
+  
+  let viewModel = MovieListViewModel(
+    topRatedMoviesUsecase: topRatedMoviesUsecase,
+    popularMoviesUsecase: popularMoviesUsecase,
+    upcomingMoviesUsecase: upcomingMoviesUsecase
+  )
+  
+  HomeViewController(viewModel: viewModel)
 })
