6edf7d33b3dc7e1b5433004b2acbe65d7baeb48d
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-18 09:41:39 +0700

    feat: implement view model of get movie detail

--- Muvi/Presentation/Detail/ViewModels/MovieDetailViewModel.swift
+++ Muvi/Presentation/Detail/ViewModels/MovieDetailViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RxSwift
+import RxRelay
+
+class MovieDetailViewModel {
+  let movieDetail: BehaviorRelay<ViewState<MovieDetail>> = BehaviorRelay(value: .initial)
+  
+  private let disposeBag = DisposeBag()
+  private let movieDetailUsecase: GetMovieDetail
+  
+  init(movieDetailUsecase: GetMovieDetail) {
+    self.movieDetailUsecase = movieDetailUsecase
+  }
+  
+  func getMovieDetail(by id: Int) async throws {
+    movieDetail.accept(.loading)
+    
+    do {
+      let items = try await movieDetailUsecase.execute(id: id)
+      
+      items.subscribe(
+        onNext: { [weak self] item in
+          guard let self else { return }
+          
+          self.movieDetail.accept(.success(item))
+        },
+        onError: { [weak self] error in
+          guard let self else { return }
+          
+          self.movieDetail.accept(.error(message: error.localizedDescription))
+        }
+      ).disposed(by: disposeBag)
+    } catch {
+      movieDetail.accept(.error(message: error.localizedDescription))
+    }
+    
+  }
+}
