14dad8580f9d754bbacc05827ab27f8e58b73532
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-22 09:18:22 +0700

    feat: create base local data source

--- Muvi/Data/Datasources/MovieLocalDataSource.swift
+++ Muvi/Data/Datasources/MovieLocalDataSource.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import RxSwift
+import CoreData
+
+protocol MovieLocalDataSource {
+  func getFavoriteMovies() -> Observable<Movie>
+}
+
+struct MovieLocalDataSourceImpl: MovieLocalDataSource {
+  
+  let database: CoreDataManager
+  var context: NSManagedObjectContext?
+  
+  init(database: CoreDataManager) {
+    self.database = database
+    self.context = database.container.viewContext
+  }
+  
+  func getFavoriteMovies() -> Observable<Movie> {
+    Observable.from([])
+  }
+  
+}
