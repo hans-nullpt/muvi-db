293f16d7e0545eeb5868a7ef11d4f1f039ed7fbc
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-22 14:14:46 +0700

    feat: create view model for favorite movies

--- Muvi/Presentation/Favorite/ViewModels/FavoriteMovieViewModel.swift
+++ Muvi/Presentation/Favorite/ViewModels/FavoriteMovieViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import RxRelay
+import RxSwift
+
+class FavoriteMovieViewModel {
+  let movies: BehaviorRelay<ViewState<[Movie]>> = BehaviorRelay(value: .initial)
+  
+  private let disposeBag = DisposeBag()
+  private let favoriteMoviesUsecase: GetFavoriteMoviesUsecase
+  
+  init(favoriteMoviesUsecase: GetFavoriteMoviesUsecase) {
+    self.favoriteMoviesUsecase = favoriteMoviesUsecase
+  }
+  
+  func getFavoriteMovies() async throws {
+    movies.accept(.loading)
+    
+    do {
+      let items = try await favoriteMoviesUsecase.execute()
+      
+      items.subscribe(
+        onNext: { [weak self] items in
+          guard let self else { return }
+          
+          self.movies.accept(.success(items))
+        },
+        onError: { [weak self] error in
+          guard let self else { return }
+          
+          self.movies.accept(.error(message: error.localizedDescription))
+        }
+      ).disposed(by: disposeBag)
+    } catch {
+      movies.accept(.error(message: error.localizedDescription))
+    }
+    
+  }
+}
