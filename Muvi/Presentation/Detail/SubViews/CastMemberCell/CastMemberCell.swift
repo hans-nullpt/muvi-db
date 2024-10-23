4bcd8627d8282585704d999c17a80b6857a8ce77
Ivan Nur Ilham Syah <ivannurilham60@gmail.com>
2024-10-23 14:23:09 +0700

    fix: cast member cell constraints, add update data func

--- Muvi/Presentation/Detail/SubViews/CastMemberCell/CastMemberCell.swift
+++ Muvi/Presentation/Detail/SubViews/CastMemberCell/CastMemberCell.swift
@@ -12,14 +12,28 @@ class CastMemberCell: UICollectionViewCell {
   static func nib() -> UINib {
     UINib(nibName: "CastMemberCell", bundle: nil)
   }
-
+  
   @IBOutlet weak var avatarView: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
   
   override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    super.awakeFromNib()
+    // Initialization code
     avatarView.layer.cornerRadius = 45
     avatarView.clipsToBounds = true
+    avatarView.layer.borderColor = UIColor.white.cgColor
+    avatarView.layer.borderWidth = 1.5
+  }
+  
+  func updateData(with data: ProductionCompanies) {
+    nameLabel.text = data.name
+    
+    if let logo = data.logoPath {
+      let url = "https://image.tmdb.org/t/p/w500\(logo)"
+      Task {
+        avatarView.image = await Helper.downloadImage(from: url) ?? UIImage(systemName: "circle")
+      }
     }
-
+  }
+  
 }
